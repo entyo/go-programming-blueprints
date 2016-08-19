f15fdff5dc8fc670ef443f29a96ea35182714a3f
e_ntyo <entyo.contact@gmail.com>
2016-08-19 15:12:57 +0900

    p161まで！

--- socialpoll/twittervotes/main.go
+++ socialpoll/twittervotes/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/bitly/go-nsq"
+
 	"labix.org/v2/mgo"
 )
 
@@ -33,4 +35,41 @@ func loadOptions() ([]string, error) {
 	}
 	iter.Close()
 	return options, iter.Err()
+}
+
+func publushVotes() <-chan struct{} {
+	stopchan := make(chan struct{}, 1)
+	pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+    go func() {
+        for vote := range votes {
+            pub.Publish("votes", []byte(vote))
+        }
+        log.Println("Publisher: 停止中です")
+        pub.Stop()
+        log.Println("Publisher: 停止しました")
+        stopchan <- struct{}{}
+    }()
+    return stopchan
+}
+
+func main() {
+    var stoplock sync.Mutex
+    stop := false
+    stopChan := make(chan struct{}, 1)
+    signalChan := make(chan os.Signal, 1)
+    go func ()  {
+        <-signalChan // <- はチャネルからの受信を試みる演算子
+        stoplock.Lock()
+        stop = true
+        stoplock.Unlock()
+        log.Println("停止します...")
+        stopChan <- struct{}{}
+        closeConn()
+    }
+    signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+    if err := dialDB(); err != nil {
+        log.Println("MongoDBへのダイヤルに失敗しました", err)
+    }
+    defer closedb()
 }
